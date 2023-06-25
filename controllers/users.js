ec3fc7c5115d9e22f917cd6d3a23450a3f0915cb
JackSheepe <teterkinvklol@mail.ru>
2023-06-26 02:27:19 +0700

    bugfixes/id2

--- controllers/users.js
+++ controllers/users.js
@@ -22,23 +22,19 @@ module.exports.getUserById = (req, res) => {
     return res.status(400).json({ message: "Неверный формат _id" });
   }
 
-  console.log("Before findOne");
-
-  return User.findOne({ _id: userId })
-    .then((user) => {
-      console.log("After findOne");
-      console.log("User:", user);
-
-      if (!user) {
+  return User.find({ _id: userId })
+    .then((users) => {
+      if (users.length === 0) {
         return res
           .status(404)
           .json({ message: "Пользователь по указанному _id не найден" });
       }
 
+      const user = users[0];
+
       return res.status(200).json({ message: "Success", user });
     })
     .catch((err) => {
-      console.log("Error:", err);
       res.status(500).send({ error: err.message });
     });
 };
@@ -47,7 +43,6 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .select("-_id")
     .then((user) => {
       const userData = {
         name: user.name,
