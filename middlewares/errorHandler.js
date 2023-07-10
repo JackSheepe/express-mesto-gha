253d92e3d12a446247d10b8e537fb7f5cbd8305a
JackSheepe <teterkinvklol@mail.ru>
2023-07-10 14:39:27 +0700

    eror

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -21,7 +21,7 @@ const errorHandler = (err, req, res, next) => {
   }
 
   res.status(customError.statusCode || 500).json({
-    message: customError.message || "Server Error",
+    message: customError.statusCode ? customError.message : "Server Error",
   });
 
   console.error(err);
